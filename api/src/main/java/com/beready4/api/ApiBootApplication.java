6ddaab96751fa91d36a767f15e9cef1ed459d436
Daejin Lim <daejin.lim@navercorp.com>
2019-12-29 22:03:37 +0900

    init project

---
+++ api/src/main/java/com/beready4/api/ApiBootApplication.java
@@ -0,0 +1,12 @@
+package com.beready4.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBootApplication.class, args);
+	}
+}
